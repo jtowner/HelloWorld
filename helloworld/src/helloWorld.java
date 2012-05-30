60940270eb72d8d9bf4964b11d5691cf7eca9b86
jtowner <jtowner@slrsd.org>
2012-05-30 14:04:56 -0400

    new change

--- helloworld/src/helloWorld.java
+++ helloworld/src/helloWorld.java
@@ -7,6 +7,9 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("got it");
+		
+			
 	}
 
 }
