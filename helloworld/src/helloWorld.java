fbd6f99ce362bb358d473dd5b21a3ccb7f398120
jtowner <jtowner@slrsd.org>
2012-05-30 14:18:09 -0400

    new stuu

--- helloworld/src/helloWorld.java
+++ helloworld/src/helloWorld.java
@@ -6,8 +6,12 @@ public class helloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Smallint smallInt = new Smallint();
 		System.out.println("Hello");
 		System.out.println("got it");
+		System.out.println(smallInt.getSmallest(2, 5));
+		
+		
 		
 			
 	}
